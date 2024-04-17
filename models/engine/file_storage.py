fd25a05f0bab370e92809802fe0915a0c5d5ac08
Elab <esmael@kali>
2024-04-17 02:21:40 -0500

    test del

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,16 @@
+import json
+from models.base_model import BaseModel
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        return {k: v for k, v in FileStorage.__objects.items() if isinstance(v, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -20,33 +25,18 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-
         try:
             with open(FileStorage.__file_path, 'r') as f:
                 serialized_objs = json.load(f)
                 for key, obj_dict in serialized_objs.items():
                     cls_name = obj_dict['__class__']
-                    cls = classes.get(cls_name)
-                    if cls:
-                        self.__objects[key] = cls(**obj_dict)
+                    cls = eval(cls_name)
+                    self.__objects[key] = cls(**obj_dict)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """Deletes an object from storage"""
+        """Deletes an object from storage if it exists"""
         if obj is not None:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             self.__objects.pop(key, None)
